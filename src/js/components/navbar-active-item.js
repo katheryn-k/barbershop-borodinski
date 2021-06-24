cd29c9ecd06ac698977fef7525bfeb3c5949982b
katheryn-k <kirichenko.ekaterynaa@yandex.ru>
2021-06-24 14:35:37 +0300

    feat: update build

--- src/js/components/navbar-active-item.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    let $active = 'navbar__item--active';
-    let $pageId = $('.header').attr('data-id-page');
-    let $activeNavItem = $(`[data-nav-${$pageId}]`);
-
-    $activeNavItem.addClass($active);
-})
-
